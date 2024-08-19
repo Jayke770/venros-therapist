5dc7009e446331bbf1d26a0e048ba59b519fed42
Jayke770 <jakevelasco40@gmail.com>
2024-08-19 18:00:23 +0800

    .

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,6 @@
 import envValid from 'envalid'
 
 export const envConfig = envValid.cleanEnv(process.env, {
-    AUTH_SECRET: envValid.str(),
-    MONGODB_URI: envValid.str()
+    AUTH_SECRET: envValid.str({ default: "" }),
+    MONGODB_URI: envValid.str({ default: "" })
 })
