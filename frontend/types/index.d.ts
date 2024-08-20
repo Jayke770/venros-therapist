d5c49bc625fe59be0952dc419d6249c8ba83e0f8
Jayke770 <jakevelasco40@gmail.com>
2024-08-20 14:59:51 +0800

    .

--- frontend/types/index.d.ts
+++ frontend/types/index.d.ts
@@ -2,15 +2,3 @@ export interface ILanguages {
     name: string,
     code: string
 }
-
-export interface IUserDataModel {
-    userType: "user" | "therapist" | "admin"
-    name: string
-    name: string,
-    dob: Date,
-    address: string,
-    email: string,
-    password: string,
-    gender: string,
-    languages: ILanguages[]
-}
