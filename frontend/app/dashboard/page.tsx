2786feb6b56a7f84c58c4d67091d28b59440d7c7
Jayke770 <jakevelasco40@gmail.com>
2024-08-23 08:12:33 +0800

    .

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -7,6 +7,7 @@ export default async function DashboardPage() {
     const cookieStore = cookies()
     const authCookie = cookieStore.get('auth')
     const session = await authHandler.getSession({ name: authCookie?.name, value: authCookie?.value })
+    console.log("fafasa", session)
     if (!session) redirect("/get-started")
     return (
         <>
