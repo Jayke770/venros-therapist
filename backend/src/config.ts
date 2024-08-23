2e517c999dad9d2ac89aced8ce54d0a11d70430f
Jayke770 <jakevelasco40@gmail.com>
2024-08-23 09:45:09 +0800

    .

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,7 @@ export const config = envalid.cleanEnv(process.env, {
     PORT: envalid.num({ default: 3000 }),
     MONGODB_URI: envalid.str(),
     JWT_SECRET: envalid.str(),
-    FRONTEND_DOMAIN: envalid.str(),
+    DOMAINS: envalid.str(),
     MINIO_ENDPOINT: envalid.str(),
     MINIO_PORT: envalid.num(),
     MINIO_ACCESSKEY: envalid.str(),
