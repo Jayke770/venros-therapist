190e490ff3e069d6a57b5278baaf5c53b2078d63
Jayke770 <jakevelasco40@gmail.com>
2024-09-04 15:49:28 +0800

    .

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ import authRoute from '@/routes/auth';
 import usersRouter from '@/routes/users'
 import { authHandler } from '@/lib/auth'
 import { helmet } from 'elysia-helmet';
-const app = new Elysia({ serve: { reusePort: true } })
+const app = new Elysia({ serve: { reusePort: true, hostname: "0.0.0.0" } })
   .use(cors({ origin: config.DOMAINS?.split(","), credentials: true }))
   .use(helmet({
     contentSecurityPolicy: {
